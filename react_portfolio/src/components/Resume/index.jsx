0c822184f27c9116ba707fc7a43bf8a9021cffa9
Khubaib Ul Haque <khubaib_12@hotmail.com>
2024-03-03 02:23:54 +0000

    - added routing for different pages and a hero for main page

--- react_portfolio/src/components/Resume/index.jsx
+++ react_portfolio/src/components/Resume/index.jsx
@@ -1,9 +1,13 @@
-function Resume(){
-    return (
-        <div>
-            <p>resume file</p>
+function Resume() {
+  return (
+    <div>
+      <div className="grid grid-nogutter surface-0 text-800">
+        <div className="col-12 md:col-6 p-6 text-center md:text-left flex align-items-center ">
+          <p>resume file</p>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Resume
+export default Resume;
--- react_portfolio/src/components/Wrapper/index.jsx
+++ react_portfolio/src/components/Wrapper/index.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 
 function Wrapper(props) {
-  return <main className="wrapper">{props.children}</main>;
+  return <main className="wrapper">
+    {props.children}    </main>;
+    
 }
 
 export default Wrapper;

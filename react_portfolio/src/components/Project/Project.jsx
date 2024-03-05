06145e4052d3676082b3de233f45ff5d93dd55c8
Khubaib Ul Haque <khubaib_12@hotmail.com>
2024-03-05 23:52:01 +0000

    - adjusting the project component

--- react_portfolio/src/components/Project/Project.jsx
+++ react_portfolio/src/components/Project/Project.jsx
@@ -10,42 +10,51 @@ function Project() {
 
   useEffect(() => {
     const project = projects.filter(
-
       (singularProject) => singularProject.id == id
     );
     setProjectData(...project);
-}, []);
-useEffect(() => {
+  }, []);
+  useEffect(() => {
     console.log("singular project ", projectData?.title);
-  }, [projectData])
-  
+  }, [projectData]);
+
   return (
-    <div >
-        
-<div className=" px-4 py-5 md:px-6 lg:px-8">
-    <div className="border-bottom-1 surface-border">
-        <span className="block text-6xl font-bold mb-4 drop-in">
-        My <span style={{ color: "var(--primary-color)" }}>{projectData?.title}</span>
-      </span>
-    </div>
-      <p>
-        Deployed Link:{" "}
-        <a href={projectData?.deployedLink} target="_blank" rel="noopener noreferrer">
-          {projectData?.deployedLink}
-        </a>
-      </p>
-      <p>
-        GitHub Link:{" "}
-        <a href={projectData?.githubLink} target="_blank" rel="noopener noreferrer">
-          {projectData?.githubLink}
-        </a>
-      </p>
-      <img src={projectData?.screenshot} alt={projectData?.title} />
-
-
-
-</div>
-      
+    <div>
+      <div className=" px-4 py-5 md:px-6 lg:px-8">
+        <div className="border-bottom-1 surface-border">
+          <span className="block text-6xl font-bold mb-4 drop-in">
+            My{" "}
+            <span style={{ color: "var(--primary-color)" }}>
+              {projectData?.title}
+            </span>
+          </span>
+        </div>
+        <p>
+          Deployed Link:{" "}
+          <a
+            href={projectData?.deployedLink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {projectData?.deployedLink}
+          </a>
+        </p>
+        <p>
+          GitHub Link:{" "}
+          <a
+            href={projectData?.githubLink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {projectData?.githubLink}
+          </a>
+        </p>
+        <img
+          src={projectData?.screenshot}
+          alt={projectData?.title}
+          style={{ width: "50%", height: "auto" }}
+        />
+      </div>
     </div>
   );
 }
